5bff6ef3c11d5a646e55a092ee74502d6cb9d09a
Michal Wegrzyn <michal.wegrzyn@pl.abb.com>
2018-12-05 16:04:48 +0100

    Use WebSQL for both Windows and Windows Phone

--- www/requireWebSql.js
+++ www/requireWebSql.js
@@ -1,4 +1,2 @@
-if (navigator.appVersion.indexOf('Windows Phone') >= 0) {
-    // Only add the WebSQL plug-in for Windows Phone
-    window.openDatabase = window.openDatabase || require('cordova-plugin-websql-async.WebSQL').openDatabase;
-}
+window.openDatabase = window.openDatabase || require('cordova-plugin-websql-async.WebSQL').openDatabase;
+
